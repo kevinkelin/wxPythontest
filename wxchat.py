8ff831db2142855e36342e5135e429b4b82eb3f9
kevinkelin <yanxingyang@gmail.com>
2015-06-19 20:03:20 +0800

    添加wxchat

---
+++ wxchat.py
@@ -0,0 +1,170 @@
+#coding:gbk
+import wx
+import optparse
+import time
+import threading
+import json,pythoncom
+import autonet,urllib
+
+#线程函数
+class FuncThread(threading.Thread):
+    def __init__(self, func, *params, **paramMap):
+        threading.Thread.__init__(self)
+        self.func = func
+        self.params = params
+        self.paramMap = paramMap
+        self.rst = None
+        self.finished = False
+
+    def run(self):
+        pythoncom.CoInitialize()
+        self.rst = self.func(*self.params, **self.paramMap)
+        self.finished = True
+        pythoncom.CoUninitialize()
+
+    def getResult(self):
+        return self.rst
+
+    def isFinished(self):
+        return self.finished
+
+def doInThread(func, *params, **paramMap):
+    ft = FuncThread(func, *params, **paramMap)
+    ft.start()
+    return ft
+
+class login(wx.Dialog):
+    def __init__(self,parent):
+        wx.Dialog.__init__(self,parent,title='登录系统',size = (300,200))
+        #初始化一个空行
+        self.blackrow = wx.StaticText(self,-1,"")
+
+        #初始化两个文本框
+        self.useridtext = wx.StaticText(self,-1,"用户ID")
+        self.passtext = wx.StaticText(self,-1,"密码")
+        self.userid = wx.TextCtrl(self,-1,'',style=wx.TE_LEFT)
+        self.password = wx.TextCtrl(self,-1,'',style=wx.TE_PASSWORD)
+
+        #初始化一个状态显示文本框
+        self.info = wx.StaticText(self,-1,'')
+        
+        #初始化三个按钮
+        self.loginBtn = wx.Button(self,-1,"登录")
+        self.Bind(wx.EVT_BUTTON,self.loginuser,self.loginBtn)
+        self.regBtn = wx.Button(self,-1,"注册")
+        self.cancelBtn = wx.Button(self,-1,"退出")
+
+        #初始化一个主窗口
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+
+        useridSizer = wx.BoxSizer(wx.HORIZONTAL)        
+        useridSizer.Add(self.useridtext,1)
+        useridSizer.Add(self.userid,2)
+
+        passSizer = wx.BoxSizer(wx.HORIZONTAL)
+        passSizer.Add(self.passtext,1)
+        passSizer.Add(self.password,2)
+
+        btnSizer = wx.BoxSizer(wx.HORIZONTAL)
+        btnSizer.Add(self.loginBtn)
+        btnSizer.Add(self.regBtn)
+        btnSizer.Add(self.cancelBtn)
+
+        mainSizer.Add((-1,20))
+        mainSizer.Add(useridSizer,2)
+        mainSizer.Add(passSizer,2)
+        mainSizer.Add(self.info)
+        mainSizer.Add(btnSizer,1)
+
+        self.SetSizer(mainSizer)
+
+        
+
+    def loginuser(self,evt):
+        userid = self.userid.GetValue()
+        userpass = self.password.GetValue()
+        self.info.SetLabel(u'正在以'+userid+u'的身份登录...')
+        body=urllib.urlencode({'userId':userid,'pswd':userpass})
+        rst = doInThread(autonet.doHttpPost,uri=r"http://pub.releaseoa.corp.qihoo.net:8385/login",body=body)
+        while True:
+            if not rst.isFinished:
+                time.sleep(0.5)
+            else:
+                print '11111111'
+                print rst.getResult()
+                break
+
+        
+            # status,headers,contents = autonet.doHttpPost(uri=r"http://pub.releaseoa.corp.qihoo.net:8385/login",body=body)
+            # print contents
+
+
+
+
+class Frame(wx.Frame): #Frame 进行初始化
+    def __init__(self,title):
+        wx.Frame.__init__(self,None,title=title,size = (300,600))       
+
+        #初始化一个菜单栏
+        menuBar = wx.MenuBar() 
+        
+        #初始化三个主菜单项
+        loginMenu = wx.Menu() 
+        talkMenu = wx.Menu()
+        helpMenu = wx.Menu()
+
+        #分别为三个主菜单项添加子菜单
+        loginMenu.Append(5001,'登录')
+        loginMenu.AppendSeparator()
+        loginMenu.Append(5005,'登出')
+        loginMenu.Append(5009,'E&xit\tAlt+X')
+
+        talkMenu.Append(6001, '检查留言')        
+
+        helpMenu.Append(7001, '关于')
+
+        #给相应的按钮添加方法
+        self.Bind(wx.EVT_MENU,self.startLogin,id=5001)
+
+        #将菜单添加到menuBar中
+        menuBar.Append(loginMenu,"登录")
+        menuBar.Append(talkMenu,"留言")
+        menuBar.Append(helpMenu,"关于")
+
+        #将menuBar设置到menu上
+        self.SetMenuBar(menuBar)
+
+        #在主恩赐初始化一个盒模型
+        boxSizer = wx.BoxSizer(wx.VERTICAL)
+        #在主界面初始化一个panel
+        self.panel = wx.Panel(self)
+        #在panel上添加一个textCtrl
+        self.infobox = wx.TextCtrl(self.panel,-1,'qqqq',style = wx.TE_MULTILINE|wx.HSCROLL,size=(300,550))
+        boxSizer.Add(self.panel,3,wx.ALL | wx.EXPAND, 3)
+
+    def startLogin(self,evt):
+        self.infobox.WriteText('start login...')
+        logDig = login(self)
+        logDig.Show()
+
+  
+class App(wx.App): ##继承wx.App
+    def OnInit(self): ##还没有调起来的时候读取初始化
+        self.frame = Frame('QA内部IM工具')        
+        self.frame.Centre()
+        self.frame.Show(True)        
+        return True
+
+def killSelf(evt = None):
+    os.system('taskkill /F /T /PID %d >NUL 2>NUL' % win32process.GetCurrentProcessId())
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('-x', '--no-update', dest = 'test', action = 'store_true', help = 'start without update')
+    parser.add_option('-t', '--no-update-test', dest = 'test2', action = 'store_true', help = 'start without update debug')
+    options, args = parser.parse_args()
+    if options.test:
+        print("-x param")
+    if options.test2:
+        print("-t param")
+    App(redirect = False).MainLoop()
--- wxtest.py
+++ wxtest.py
@@ -39,8 +39,7 @@ def doInThread(func, *params, **paramMap):
 
 class Frame(wx.Frame): #Frame 进行初始化
     def __init__(self,title):
-        wx.Frame.__init__(self,None,title=title,size = (900,700))
-        boxSizer = wx.BoxSizer(wx.VERTICAL) #初始化一个boxSizer模型
+        wx.Frame.__init__(self,None,title=title,size = (900,700))       
 
         #初始化一个菜单栏
         menuBar = wx.MenuBar() 
@@ -76,12 +75,31 @@ class Frame(wx.Frame): #Frame 进行初始化
         #将menuBar添加到主Frame中
         self.SetMenuBar(menuBar)
 
+        boxSizer = wx.BoxSizer(wx.VERTICAL) #初始化一个boxSizer模型
         #初始化一个panel
         self.panel = wx.Panel(self)
-        boxSizer.Add(self.panel,3,wx.ALL|wx.EXPAND,3)
+        boxSizer.Add(self.panel,3,wx.ALL|wx.EXPAND,3) #前面的3表示和同组元素的所占比，后面的3是border宽
 
         #初始化一个textCtrl,StaticText 需要放到一个panel中，也就是上面创建的那个panel
+        #初始化组件的时候，要给组件加上一个ID，不关心ID的话可以使用-1，并且还要加上一个父类，没有可以使用None
+        #有些组件必须要有个父类的
         self.output = wx.TextCtrl(self.panel,-1,'',style = wx.TE_MULTILINE|wx.HSCROLL, size=(800, 600))
+        
+        #初始化两个button的实例，一个保存，一个是关闭 
+        self.saveBtn = wx.Button(self,-1,'保存') 
+        
+        self.cancelBtn = wx.Button(self, wx.ID_CANCEL, '关闭')
+        canelBtn2 = wx.Button(self,-1,'关闭2') 
+        self.Bind(wx.EVT_BUTTON,self.onClose,id = wx.ID_CANCEL)
+        self.Bind(wx.EVT_BUTTON,self.onClose,canelBtn2)
+        #底部初始化一个的btnSizer，然后将两个按钮放到这个sizer中
+        btnSizer = wx.BoxSizer(wx.HORIZONTAL)        
+        btnSizer.Add(self.saveBtn,0,wx.ALIGN_CENTER_VERTICAL)
+        btnSizer.Add(self.cancelBtn,0,wx.ALIGN_CENTER_VERTICAL)
+        btnSizer.Add(canelBtn2,0,wx.ALIGN_CENTER_VERTICAL)
+        
+        boxSizer.Add(btnSizer, 0, wx.ALIGN_CENTER, 3)
+        self.SetSizer(boxSizer)
 
     def ShowAbout(self,evt):#事件函数要传一个evt(事件)为参数
     	wx.MessageDialog(self,'wxPython使用介绍','提示',wx.OK|wx.ICON_INFORMATION).ShowModal()
@@ -99,8 +117,14 @@ class Frame(wx.Frame): #Frame 进行初始化
         doInThread(wait)
 
 
+    def onClose(self,evt):
+        self.output.AppendText('close...\r\n')
+        time.sleep(1)
+        self.Destroy()
 
-class App(wx.App): ##继承wx,App
+
+
+class App(wx.App): ##继承wx.App
     def OnInit(self): ##还没有调起来的时候读取初始化
         self.frame = Frame('修改host工具')        
         self.frame.Centre()
